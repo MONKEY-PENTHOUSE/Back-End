358b7704a5324390e72e52fe33083bda56b3372b
Resilient923 <kc960923@naver.com>
2021-06-25 14:04:07 +0900

    [update] middleware 210625

--- config/jwtMiddleware.js
+++ config/jwtMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const secret_config = require('./secret');
 // 미들웨어 만들어줘서 사용
 const jwtMiddleware = (req, res, next) => {
-    // read the token from header or url
+    //url로 부터 토큰을 읽어들인다.
     const token = req.headers['x-access-token'] || req.query.token;
     // token does not exist
     if(!token) {
@@ -13,8 +13,7 @@ const jwtMiddleware = (req, res, next) => {
             message: '로그인이 되어 있지 않습니다.'
         });
     }
-
-    // create a promise that decodes the token
+    //비동기작업들을 순차적으로 진행해주는 promise를 생성하고 토큰을 해독하는 함수를 넣는다.
     const p = new Promise(
         (resolve, reject) => {
             jwt.verify(token, secret_config.jwtsecret , (err, verifiedToken) => {
@@ -24,7 +23,7 @@ const jwtMiddleware = (req, res, next) => {
         }
     );
 
-    // if it has failed to verify, it will return an error message
+    // 토큰 유효성검사 실패시, 검증실패 res를 뿌린다.
     const onError = (error) => {
         res.status(403).json({
             isSuccess:false,
@@ -33,7 +32,7 @@ const jwtMiddleware = (req, res, next) => {
         });
     };
 
-    // process the promise
+    // promise과정
     p.then((verifiedToken)=>{
         //비밀 번호 변경됐을 때 검증 부분 추가 할 곳
         req.verifiedToken = verifiedToken;
--- config/winston.js
+++ config/winston.js
@@ -6,6 +6,16 @@ const env = process.env.NODE_ENV || 'development';
 const logDir = 'log';
 
 // https://lovemewithoutall.github.io/it/winston-example/
+//콘솔창에 로그를 남긴다.
+//콘솔창의 로그는 컬러로 찍힌다.
+//로그 파일을 남긴다.
+//콘솔창은 info 레벨부터 로그를 남기고, 로그 파일에는 debug 레벨부터 로그를 남긴다.
+//로그 파일은 매일 하나씩 남긴다.
+//log 폴더에 로그 파일을 쌓는다.
+//로그 파일은 최대 20Mbyte다.
+//로그 파일은 14일간 보관한다.
+//로그 파일은 압축한다.
+//개발 환경에서는 debug 레벨부터 로그를 찍고, 운영 환경에서는 info 레벨부터 로그를 찍는다.
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir)
