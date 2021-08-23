d23dd082512114c5ea16ac3b106a1674257a1076
Resilient923 <kc960923@naver.com>
2021-08-24 00:00:58 +0900

    [update] sequelizeUserModel 210824

--- models/user.js
+++ models/user.js
@@ -1,24 +1,67 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  user.init({
-    user_id: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
-  return user;
-};
+const Sequelize = require('sequelize');
+// const TrustedComms = require('twilio/lib/rest/preview/TrustedComms');
+
+module.exports = class User extends Sequelize.Model{
+  static init(sequelize){
+    return super.init({
+      userIdx:{
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      userName:{
+        type: Sequelize.TEXT,
+        allowNull : false
+      },
+      userRoomNumber:{
+        type: Sequelize.TEXT,
+        allowNull : false
+      },
+      userPassword:{
+        type: Sequelize.TEXT,
+        allowNull : false
+      },
+      userPasswordSalt:{
+        type: Sequelize.TEXT,
+        allowNull : false
+      },
+      userEmail:{
+        type: Sequelize.TEXT,
+        allowNull : false
+      },
+      userPhone:{
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      userSex:{
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      userBirth:{
+        type:Sequelize.DATE,
+        allowNull : false
+      },
+      userDelete:{
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      userLoginMethod:{
+        type : Sequelize.STRING,
+        allowNull : false
+      },
+      // userCreatedAt:{
+      //   type:Sequelize.DATE,
+      //   allowNull : false,
+      //   defaultValue : Sequelize.NOW
+      // }
+    },{
+      sequelize,
+      timestamps:true,
+      modelName:'users',
+      tableName:'users',
+      paranoid:true,
+      charset:'utf8',
+      collate:'utf8_general_ci'
+    })
+  }
+}
+
